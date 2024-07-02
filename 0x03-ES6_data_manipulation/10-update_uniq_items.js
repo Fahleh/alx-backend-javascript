2827f6f12e4f0d518ae980bb4247b9a2c873584b
Fahleh <eddyukpong@gmail.com>
2024-07-02 14:06:14 +0100

    Fixes

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,6 +1,6 @@
 // Returns an updated map for all items with initial quantity at 1.
 export default function updateUniqueItems(map) {
-  if (!(map instanceof Map)) throw new Error('Argument must be a Map');
+  if (!(map instanceof Map)) throw new Error('Cannot process');
 
   map.forEach((value, key) => {
     if (value === 1) {

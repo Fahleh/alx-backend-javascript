3e15a22dc456b08c5d22d68a2aa3ca8e0ad01c90
Fahleh <eddyukpong@gmail.com>
2024-07-02 13:33:46 +0100

    Fixes

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,10 +1,10 @@
-export const weakMap = new WeakMap();
+const weakMap = new WeakMap();
 
-export function queryAPI(endpoint) {
-  if (weakMap.get(endpoint) >= 5) throw new Error('Endpoint load is high');
+const queryAPI = (endpoint) => {
+  let total = weakMap.get(endpoint) || 0;
+  weakMap.set(endpoint, total -= -1);
+  if (total >= 5) throw new Error('Endpoint load is high');
+  return total;
+};
 
-  if (!weakMap.has(endpoint)) {
-    weakMap.set(endpoint, 0);
-  }
-  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
-}
+export { weakMap, queryAPI };

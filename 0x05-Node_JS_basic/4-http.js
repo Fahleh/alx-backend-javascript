c565e9bc33348e39b261c3b031194fba5af5150e
Fahleh <eddyukpong@gmail.com>
2024-08-21 00:46:38 +0100

    Fixes

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,11 +1,11 @@
-const http = require("http");
+const http = require('http');
 
-const host = "127.0.0.1";
+const host = '127.0.0.1';
 const port = 1245;
 
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.end("Hello Holberton School!");
+  res.end('Hello Holberton School!');
 });
 
 app.listen(port, host, () => {

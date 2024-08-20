61661597d12b57986fa50a114407d687a40a6953
Fahleh <eddyukpong@gmail.com>
2024-08-21 00:59:17 +0100

    Fixes

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,15 +1,15 @@
-const express = require("express");
-const router = require("./routes/index");
+const express = require('express');
+const router = require('./routes/index');
 
 const app = express();
 const PORT = 1245;
 
-app.use("/", router);
-app.use("/students", router);
-app.use("/students/:major", router);
+app.use('/', router);
+app.use('/students', router);
+app.use('/students/:major', router);
 
 app.listen(PORT, () => {
   console.log(`Listening at http://localhost:${PORT}`);
 });
 
-module.exPORTs = app;
+module.exports = app;
--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,23 +1,23 @@
-const fs = require("fs");
+const fs = require('fs');
 
 const readDatabase = (path) =>
   new Promise((resolve, reject) => {
     fs.readFile(path, (error, csvData) => {
       if (error) {
-        reject(Error("Cannot load the database"));
+        reject(Error('Cannot load the database'));
       }
       if (!csvData) {
         return;
       }
       const fieldsObj = {};
       const response = {};
-      let data = csvData.toString().split("\n");
+      let data = csvData.toString().split('\n');
       data = data.filter((element) => element.length > 0);
 
       data.shift();
       data.forEach((element) => {
         if (element.length > 0) {
-          const row = element.split(",");
+          const row = element.split(',');
           if (row[3] in fieldsObj) {
             fieldsObj[row[3]].push(row[0]);
           } else {
@@ -29,7 +29,7 @@ const readDatabase = (path) =>
         if (field) {
           const list = fieldsObj[field];
           response[field] = {
-            list: `List: ${list.toString().replace(/,/g, ", ")}`,
+            list: `List: ${list.toString().replace(/,/g, ', ')}`,
             number: list.length,
           };
         }

86b8ebb932d5f862cd301a12894e56d947e85dfa
Fahleh <eddyukpong@gmail.com>
2024-08-21 00:37:44 +0100

    Fixes

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,17 +1,17 @@
-const fs = require("fs");
+const fs = require('fs');
 
 function countStudents(path) {
   try {
-    const fileData = fs.readFileSync(path, { encoding: "utf8" }).split(/\r?\n/);
+    const fileData = fs.readFileSync(path, { encoding: 'utf8' }).split(/\r?\n/);
     const fileLines = fileData;
     let i = 0;
     let countStudents = 0;
     const fieldsObj = {};
 
     for (const ln of fileLines) {
-      if (ln.trim() !== "" && i > 0) {
+      if (ln.trim() !== '' && i > 0) {
         countStudents += 1;
-        const [firstName, lastName, age, field] = ln.split(","); // eslint-disable-line
+        const [firstName, lastName, age, field] = ln.split(','); // eslint-disable-line
         if (!fieldsObj[field]) {
           fieldsObj[field] = {
             count: 1,
@@ -32,13 +32,13 @@ function countStudents(path) {
     console.log(`Number of students: ${countStudents}`);
     for (const field of Object.keys(fieldsObj)) {
       const len = fieldsObj[field].count;
-      const studentNames = fieldsObj[field].students.join(", ");
+      const studentNames = fieldsObj[field].students.join(', ');
       console.log(
         `Number of students in ${field}: ${len}. List: ${studentNames}`
       );
     }
   } catch (error) {
-    throw new Error("Cannot load the database");
+    throw new Error('Cannot load the database');
   }
 }
 
